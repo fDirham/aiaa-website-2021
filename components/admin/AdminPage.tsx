9fe5f9f2eff2d19c508c8d82f1a568449cd287c5
fbd <fajarletters@gmail.com>
2021-10-07 17:38:56 -0700

    admin fix

---
+++ components/admin/AdminPage.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef, useState } from "react";
+import styles from "styles/Admin.module.scss";
+import { signOut } from "firebase/auth";
+import { firebaseAuth } from "firebaseApp";
+import AddEvent from "components/admin/AddEvent";
+import SignIn from "components/admin/SignIn";
+
+export default function AdminPage() {
+  const [authenticated, setAuthenticated] = useState<boolean>(false);
+  const checkAuthTimer = useRef<ReturnType<typeof setInterval>>();
+  const { currentUser } = firebaseAuth;
+  useEffect(() => {
+    if (!authenticated) {
+      checkAuthTimer.current = setInterval(() => {
+        if (firebaseAuth.currentUser) setAuthenticated(true);
+      }, 1000);
+    } else {
+      if (checkAuthTimer.current) clearInterval(checkAuthTimer.current);
+    }
+
+    return () => {
+      if (checkAuthTimer.current) clearInterval(checkAuthTimer.current);
+    };
+  }, [authenticated]);
+
+  async function handleSignOut() {
+    await signOut(firebaseAuth);
+    setAuthenticated(false);
+  }
+
+  if (!currentUser) return <SignIn />;
+  return (
+    <div className={styles.container}>
+      Admin Portal
+      <button onClick={handleSignOut}>Sign Out</button>
+      <AddEvent />
+    </div>
+  );
+}
